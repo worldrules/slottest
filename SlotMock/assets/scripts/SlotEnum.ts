eafd17454c34baa51a481ea62fd6ce5efef3667c
worldrules <worldrulesdev@gmail.com>
2021-06-09 18:07:47 -0300

    SlotEnum Create

--- SlotMock/assets/scripts/SlotEnum.ts
+++ SlotMock/assets/scripts/SlotEnum.ts
@@ -5,7 +5,23 @@ enum Direction {
   Down,
 }
 
+enum Configuration {
+  Random,
+  Single,
+  Double,
+  All,
+}
+enum Lines {
+  Upper,
+  Middle,
+  Bottom,
+}
+
 @ccclass
 export default class SlotEnum extends cc.Component {
   static Direction = Direction;
+
+  static Configuration = Configuration;
+
+  static Lines = Lines;
 }
